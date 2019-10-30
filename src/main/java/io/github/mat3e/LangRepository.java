015ea3cd6cfe016fe99c85ed6b6c3538561732b7
Your Name <you@example.com>
2019-10-30 14:10:43 +0100

    Add Lang(languages) class and LangRepository class, no implementation

---
+++ src/main/java/io/github/mat3e/LangRepository.java
@@ -0,0 +1,20 @@
+package io.github.mat3e;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+class LangRepository {
+    private List<Lang> languages;
+
+    LangRepository(){
+        languages = new ArrayList<>();
+        languages.add(new Lang(1L, "Hello", "en"));
+        languages.add(new Lang(2L, "Siema", "pl"));
+    }
+    Optional<Lang> findById(Lang id){
+        return languages.stream()
+                .filter(l -> l.getId().equals(id))
+                .findFirst();
+    }
+}
