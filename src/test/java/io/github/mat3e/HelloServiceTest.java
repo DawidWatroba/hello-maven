a675b8811f47c98771d73cf784fbda68ceb75720
Your Name <you@example.com>
2019-11-06 12:11:27 +0100

    Add lang_parameter in HelloServlet, improve tests

--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -2,33 +2,49 @@ package io.github.mat3e;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class HelloServiceTest {
-    private HelloService SUT = new HelloService();
+    private final static String WELCOME = "Hello";
 
     @Test
-    public void test_null_prepareGreeting_returnsGreetingWithFallbackValue() throws Exception {
+    public void test_prepareGreeting_nullName_returnsGreetingWithFallbackName() throws Exception {
         //given
+        var mockRepository = alwaysReturningHelloRepository();
         String name = null;
+        var SUT = new HelloService(mockRepository);
 
         //when
-        var result = SUT.prepareGreeting(name);
+        var result = SUT.prepareGreeting(null, "-1");
 
         //then
-        assertEquals("Hello " + HelloService.FALLBACK_NAME + "!", result);
+        assertEquals(WELCOME+" " + HelloService.FALLBACK_NAME + "!", result);
     }
+
 
     @Test
     public void test_name_prepareGreeting_returnsGreetingWithName() throws Exception {
         //given
         String name = "test";
+        var SUT = new HelloService();
 
         //when
-        var result = SUT.prepareGreeting(name);
+        var result = SUT.prepareGreeting(name, "-1");
 
         //then
-        assertEquals("Hello " + name + "!", result);
+        assertEquals(WELCOME+" " + name + "!", result);
+    }
+
+
+    private LangRepository alwaysReturningHelloRepository() {
+        return new LangRepository(){
+            @Override
+            Optional<Lang> findById(Long id) {
+                return Optional.of(new Lang(null, WELCOME, null));
+            }
+        };
     }
 }
